13e49bb6691bb017514f2b67ef3b044b43830dfc
danielscarlazzetta <daniel.scarlazzetta@gmail.com>
2024-08-26 15:07:25 -0400

    create a routes, and struct folder

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+import { title_font } from "@/config/fonts";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1>Login page</h1>
+    </main>
+  );
+}
---
+++ src/app/auth/new-account/page.tsx
@@ -0,0 +1,10 @@
+import { title_font } from "@/config/fonts";
+import Image from "next/image";
+
+export default function () {
+  return (
+    <main className="">
+      <h1>New account</h1>
+    </main>
+  );
+}
