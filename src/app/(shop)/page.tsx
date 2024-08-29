eda143dcec7e6a6444257a07eee62b18f623971e
danielscarlazzetta <daniel.scarlazzetta@gmail.com>
2024-08-28 20:20:12 -0400

    rutas de shop

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
+    <div className="">
       <h1>Wenaza</h1>
       <h1 className={`${title_font.className} font-bold`}>Wenaza</h1>
       <h1 className={title_font.className}>Wenaza</h1>
-    </main>
+    </div>
   );
 }
