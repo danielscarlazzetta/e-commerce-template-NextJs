60336434e5a81c7bb285cbc734c422c81a16f476
danielscarlazzetta <daniel.scarlazzetta@gmail.com>
2024-09-03 20:57:17 -0400

    create a side menu bar

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export * from './ui/top-menu/Top-menu';
 export * from './ui/not-found/PageNotFound';
 export * from './ui/title/Title';
+export * from './ui/sidebar/Sidebar';
 
 export * from './products/product-grid/ProductGrid'
 export * from './products/product-grid/ProductGridItem'
