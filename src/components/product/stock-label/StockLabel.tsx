f02554a2dc47239e71e89020d77fbea0d1c030b9
danielscarlazzetta <daniel.scarlazzetta@gmail.com>
2024-09-16 23:49:40 -0300

    create a function sleep, with skeleton products, stock

--- src/components/product/stock-label/StockLabel.tsx
+++ src/components/product/stock-label/StockLabel.tsx
@@ -6,30 +6,47 @@ import { title_font } from "@/config/fonts"
 import { useEffect, useState } from "react";
 
 
-interface Props{
- slug: string;
+interface Props {
+    slug: string;
 }
 
 
-export const StockLabel = ( {slug}: Props) => {
+export const StockLabel = ({ slug }: Props) => {
 
     const [stock, setStock] = useState(0);
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         getStock();
-    },[])
+    }, [])
 
-    const getStock = async() => {
+    const getStock = async () => {
         const inStock = await getStockBySlug(slug)
         setStock(inStock);
+        setIsLoading(false);
     }
 
 
 
     return (
-        <h5 className={` ${title_font.className} antialiased font-bold text-sm`}>
-            Stock: {stock}
-        </h5>
+        <>
+            {
+                isLoading
+                    ?
+                    (
+                        <h5 className={` ${title_font.className} bg-pink-300 animate-pulse`}>
+                            &nbsp;
+                        </h5>
+                    ) :
+                    (
+                        <h5 className={` ${title_font.className} antialiased font-bold text-sm`}>
+                            Stock: {stock}
+                        </h5>
+                    )
+
+            }
+
+
+        </>
     )
 }
