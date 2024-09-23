497c83f55c67346426d248c083fd8e300b79bf3f
danielscarlazzetta <daniel.scarlazzetta@gmail.com>
2024-09-23 18:31:53 -0300

    create a interface with cart

---
+++ src/store/cart/cart-store.ts
@@ -0,0 +1,56 @@
+import type { CartProduct } from "@/interface";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+
+
+interface State {
+    cart: CartProduct[];
+
+    addProductToCart: (product: CartProduct) => void;
+    //updateProductQuantity
+    //removeProduct
+}
+
+export const useCartStore = create<State>()(
+
+    // persistencia en el carro de compras
+    persist(
+        (set, get) => ({
+
+            cart: [],
+            //metodos
+            addProductToCart: (product: CartProduct) => {
+                const { cart } = get();
+
+                // 1. revisar si existe el producto
+                const productInCart = cart.some(
+                    (item) => (item.id === product.id && item.size === product.size)
+                );
+
+                if (!productInCart) {
+                    set({ cart: [...cart, product] })
+                    return
+                }
+
+                // 2. el producto existe por talla, ahora se incrementa
+                const updatedCartProduct = cart.map((item) => {
+                    if (item.id === product.id && item.size === product.size) {
+                        return { ...item, quantity: item.quantity + product.quantity }
+                    }
+
+                    return item;
+                });
+
+                set({ cart: updatedCartProduct })
+            }
+
+        })
+        ,
+
+        { 
+            name: "shopping-cart",
+            // skipHydration: true,
+        },
+    )
+)
