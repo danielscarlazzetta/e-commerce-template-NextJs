141f73cbddb497179d0975eeb6902d072b343fa1
danielscarlazzetta <daniel.scarlazzetta@gmail.com>
2024-09-25 02:05:10 -0300

    create a experimental grid product

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -12,28 +12,57 @@ interface Props {
 
 export const ProductGridItem = ({ product }: Props) => {
 
-    const [displayImage, setDisplayImage] = useState( product.images[0]);
+    const [displayImage, setDisplayImage] = useState(product.images[0]);
 
     return (
-        <div className="rounded-md overflow-hidden fade-in">
-            <Link href={`/product/${product.slug}`}>
-                <Image
-                    src={`/products/${ displayImage}`}
-                    alt={product.title}
-                    className="w-full object-cover rounded"
-                    width={500}
-                    height={500}
-                    onMouseEnter={ () => setDisplayImage( product.images[1] )}
-                    onMouseLeave={ () => setDisplayImage( product.images[0] )}/>
-            </Link>
+        // <div className="rounded-md overflow-hidden fade-in">
+        //     <Link href={`/product/${product.slug}`}>
+        //         <Image
+        //             src={`/products/${ displayImage}`}
+        //             alt={product.title}
+        //             className="w-full object-cover rounded"
+        //             width={500}
+        //             height={500}
+        //             onMouseEnter={ () => setDisplayImage( product.images[1] )}
+        //             onMouseLeave={ () => setDisplayImage( product.images[0] )}/>
+        //     </Link>
 
-            <div className="p-4 flex flex-col">
-                <Link className="hover:text-pink-600 hover:font-bold transition-all" 
-                href={`/product/${product.slug}`}>
-                    {product.title}
-                </Link>
-                <span className="font-bold">{product.price}</span>
-            </div>
+        //     <div className="p-4 flex flex-col">
+        //         <Link className="hover:text-pink-600 hover:font-bold transition-all" 
+        //         href={`/product/${product.slug}`}>
+        //             {product.title}
+        //         </Link>
+        //         <span className="font-bold">{product.price}</span>
+        //     </div>
+        // </div>
+
+
+        // Todo esto es expiremental, esta sujeto a cambios! junto con el globalCss
+
+        //<div className="rounded-md overflow-hidden fade-in">
+        <div className="rounded fade-in my-8">
+            <Link href={`/product/${product.slug}`}>
+                <div className="book">
+                    <div className="p-4 flex flex-col">
+                        <Link className="hover:text-pink-600 hover:font-bold transition-all"
+                            href={`/product/${product.slug}`}>
+                            {product.title}
+                        </Link>
+                        <span className="font-bold text-center mt-20">{product.price}</span>
+                    </div>
+
+                    {/* <button className="mb-1" type="button">Agregar al carro</button> */}
+                    <div className="fade transition-all">
+                        <Image
+                            src={`/products/${displayImage}`}
+                            alt={product.title}
+                            className="w-full object-cover rounded imgBook"
+                            width={500}
+                            height={500}
+                        />
+                    </div>
+                </div>
+            </Link>
         </div>
     );
 }
