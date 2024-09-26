093e881b96541f2bf8871a040fe9c10419969d22
danielscarlazzetta <daniel.scarlazzetta@gmail.com>
2024-09-26 01:17:08 -0300

    create a exp user in formlogin

--- src/actions/auth/Login.ts
+++ src/actions/auth/Login.ts
@@ -1,16 +1,18 @@
 'use server';
  
 import { signIn } from '@/auth.config'
+import { sleep } from '@/utils';
  
 export async function authenticate(
     prevState: string | undefined,
     formData: FormData,
   ) {
     try {
-        console.log( { formData: Object.fromEntries(formData)})
+
+        await sleep(.2);
         await signIn('credentials', Object.fromEntries(formData));
-        return 'Success';
-        
+        // return 'Success';
+
     } catch (error) {
         // if((error as Error).message.includes('CredentialsSignin')){
         //     return 'CredentialsSignin'
--- src/app/auth/login/ui/LoginFomr.tsx
+++ src/app/auth/login/ui/LoginFomr.tsx
@@ -1,56 +1,90 @@
 'use client'
 
 import { authenticate } from "@/actions";
+import clsx from "clsx";
 import Link from "next/link"
-import { useFormState } from "react-dom";
+import { useFormState, useFormStatus } from "react-dom";
+import { IoInformationOutline } from "react-icons/io5";
 
 export const LoginForm = () => {
 
-    const [state, dispatch] = useFormState(
-        authenticate,
-        undefined,
-      );
+  const [state, dispatch] = useFormState(
+    authenticate,
+    undefined,
+  );
 
-      console.log({state});
-      console.log('Login form' , {state});
+  console.log({ state });
+  console.log('Login form', { state });
 
-    return(
-        <form action={ dispatch } className="flex flex-col">
+  return (
+    <form action={dispatch} className="flex flex-col">
 
-        <label htmlFor="email">Correo electrónico</label>
-        <input
-          className="px-5 py-2 border bg-gray-200 rounded mb-5"
-          type="email"
-          name="email"
-          />
-
-
-        <label htmlFor="email">Contraseña</label>
-        <input
-          className="px-5 py-2 border bg-gray-200 rounded mb-5"
-          type="password"
-          name="password" />
-
-        <button
-            type="submit"
-          className="btn-primary">
-          Ingresar
-        </button>
+      <label htmlFor="email">Correo electrónico</label>
+      <input
+        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        type="email"
+        name="email"
+      />
 
 
-        {/* divisor l ine */ }
-        <div className="flex items-center my-5">
-          <div className="flex-1 border-t border-gray-500"></div>
-          <div className="px-2 text-gray-600">O</div>
-          <div className="flex-1 border-t border-gray-500"></div>
-        </div>
+      <label htmlFor="email">Contraseña</label>
+      <input
+        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        type="password"
+        name="password" />
+      {
+        state === "CredentialsSignin" && (
+          <div className="mb-5 flex flex-row">
 
-        <Link
-          href="/auth/new-account" 
-          className="btn-secondary text-center">
-          Crear una nueva cuenta
-        </Link>
+            <IoInformationOutline className="h-5 w-5 text-red-500" />
+            <p className="text-sm text-red-500">
+              Credenciales invalidas
+            </p>
+          </div>
+        )
+      }
+      {/* <button
+        type="submit"
+        className="btn-primary">
+        Ingresar
+      </button> */}
 
-      </form>
-    )
+      <LoginButton />
+
+
+      {/* divisor l ine */}
+      <div className="flex items-center my-5">
+        <div className="flex-1 border-t border-gray-500"></div>
+        <div className="px-2 text-gray-600">O</div>
+        <div className="flex-1 border-t border-gray-500"></div>
+      </div>
+
+
+      <Link
+        href="/auth/new-account"
+        className="btn-secondary text-center">
+        Crear una nueva cuenta
+      </Link>
+
+    </form>
+  )
+}
+
+function LoginButton() {
+  const { pending } = useFormStatus();
+
+  return(
+    <button
+        type="submit"
+        className={
+          clsx(
+            {"btn-primary": !pending,
+            "btn-disabled": pending}
+          )
+        }
+        disabled={pending}
+        >
+        Ingresar
+      </button>
+  )
 }
